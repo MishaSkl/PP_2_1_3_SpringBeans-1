c8f77bc0947977049c6931a1dfecbafb5c826dca
MishaSkl <112400829+MishaSkl@users.noreply.github.com>
2022-10-22 13:23:14 +0300

    1.0.0

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -3,7 +3,10 @@ package app.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+
 }
